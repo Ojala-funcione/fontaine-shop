8a2f89fda0ed9a03929f9b40ac22329e595b3079
leoruhl94 <leoruhl94@gmail.com>
2022-04-04 19:37:13 -0300

    .

---
+++ src/common/logo/logo.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Router from 'next/router';
+import { Box } from '@chakra-ui/react';
+
+type LogoProps = {
+  // imageUrl: string;
+  // alt: string;
+  // onClick?: () => void;
+};
+
+const Logo: React.FC<LogoProps> = () => {
+  return <Box>Logo</Box>;
+};
+
+export default Logo;
---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+// import Router, { useRouter } from 'next/router';
+
+type Props = {};
+
+const Header: React.FC<Props> = () => {
+  return <Box>Header</Box>;
+};
+
+export default Header;
