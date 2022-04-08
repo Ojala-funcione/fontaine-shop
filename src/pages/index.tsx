beb9ad3aff4260d3e1a5d2fb6dadeaa47a5b8117
Jime227 <jamedina@live.com.ar>
2022-04-07 21:14:17 -0300

    config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-
-// const hola = 'nada';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { db } from "../../firebase";
+import {
+  query,
+  doc,
+  deleteDoc,
+  setDoc,
+  collection,
+  updateDoc,
+  getDocs,
+  getDoc,
+  where,
+  addDoc,
+} from "firebase/firestore";
+const getAllProducts = async () => {
+  try {
+    const q = query(collection(db, "products"));
+    const querySnapshot = await getDocs(q);
+    let allProducts = querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    return allProducts;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
 
 const Home: NextPage = () => {
   return (
@@ -14,42 +39,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to
-          <a href="https://nextjs.org"> Next.js!</a>
-        </h1>
+      <h1 className={styles.title}>Welcome to ...home?</h1>
 
-        <p className={styles.description}>
-          Get started by editing
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card}>
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
+      <div className={styles.card}>hola </div>
 
       <footer className={styles.footer}>
-        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Powered by
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
