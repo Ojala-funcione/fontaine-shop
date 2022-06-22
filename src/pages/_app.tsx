10fccc16df4d999210b803fb98b794608af331a9
leoruhl94 <leoruhl94@gmail.com>
2022-06-21 21:06:45 -0300

    v11 pages CRLF error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import '../src/styles/globals.css';
+import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { AuthProvider } from '@context/useAuth';
